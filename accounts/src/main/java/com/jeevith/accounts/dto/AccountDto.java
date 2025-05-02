8a6fe3cda9776b5619484221be5c005c3f0e249d
Jeevith-U <102320065+Jeevith-U@users.noreply.github.com>
2025-05-02 23:42:17 +0530

    Altered the DTO for Better Documentation

--- accounts/src/main/java/com/jeevith/accounts/dto/AccountDto.java
+++ accounts/src/main/java/com/jeevith/accounts/dto/AccountDto.java
@@ -1,5 +1,6 @@
 package com.jeevith.accounts.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.validation.constraints.NotEmpty;
@@ -13,18 +14,22 @@ import lombok.Setter;
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
-
+@Schema(
+        name = "Accounts",
+        description = "Schema to hold Account information"
+)
 public class AccountDto {
 
     @NotEmpty(message = "AccountNumber can not be a null or empty")
     @Pattern(regexp="(^$|[0-9]{10})",message = "AccountNumber must be 10 digits")
+    @Schema(description = "Account Number Bank account", example = "3454433243")
     private Long accountNumber;
 
     @NotEmpty(message = "AccountType can not be a null or empty")
-
+    @Schema(description = "Account type Bank account", example = "Savings")
     private String accountType;
 
     @NotEmpty(message = "BranchAddress can not be a null or empty")
-
+    @Schema(description = "Bank Address", example = "San's Heart")
     private String branchAddress;
 }
--- accounts/src/main/java/com/jeevith/accounts/dto/CustomerDto.java
+++ accounts/src/main/java/com/jeevith/accounts/dto/CustomerDto.java
@@ -1,6 +1,7 @@
 package com.jeevith.accounts.dto;
 
 import com.jeevith.accounts.utility.GenerateCustomCustomerId;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.Entity;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotEmpty;
@@ -19,13 +20,16 @@ public class CustomerDto {
 
     @NotEmpty(message = "Name can not be a null or empty")
     @Size(min = 5, max = 30, message = "The length of the customer name should be between 5 and 30")
+    @Schema(description = "Customer Name", example = "Jeevith")
     private String customerName;
 
     @NotEmpty(message = "Email address can not be a null or empty")
     @Email(message = "Email address should be a valid value")
+    @Schema(description = "Customer Email", examples = {"jeevith.u.work@gmail.com", "jeev2@outlook.com"})
     private String customerEmail;
 
     @Pattern(regexp = "(^$|[0-9]{10})", message = "Mobile number must be 10 digits")
+    @Schema(description = "Customer Phone Number", example = "9876543210")
     private String customerPhoneNumber;
 
     private AccountDto accountsDto;
