640d3e079b2bb2d97820c701209552b707b02e0e
Jeevith-U <102320065+Jeevith-U@users.noreply.github.com>
2025-05-03 16:07:24 +0530

    Card Project Initialization

---
+++ cards/src/main/java/com/jeevith/cards/entity/Cards.java
@@ -0,0 +1,31 @@
+package com.jeevith.cards.entity;
+
+import com.jeevith.cards.util.GenarateCustomeCardId;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+public class Cards extends BaseEntity {
+
+	@Id
+	@GenarateCustomeCardId
+	private Long cardId;
+
+	private String mobileNumber;
+
+	private String cardNumber;
+
+	private String cardType;
+
+	private int totalLimit;
+
+	private int amountUsed;
+
+	private int availableAmount;
+	
+}
