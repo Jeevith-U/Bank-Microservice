349fcc6219d6f310c401a2c886c2d725b1d9834f
Jeevith-U <102320065+Jeevith-U@users.noreply.github.com>
2025-05-27 12:00:13 +0530

    Started with the messaging service using pub & subs model using RabbitMQ

---
+++ message/src/main/java/com/jeevith/message/dto/AccountsMsgDto.java
@@ -0,0 +1,4 @@
+package com.jeevith.message.dto;
+
+public record AccountsMsgDto(String accountNumber, String name, String email, String mobileNumber) {
+}
---
+++ message/src/main/java/com/jeevith/message/functions/MessageFunction.java
@@ -0,0 +1,9 @@
+package com.jeevith.message.functions;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MessageFunction {
+
+
+}
